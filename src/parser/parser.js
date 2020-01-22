e492f853f714c8845200766b69d1b686e614a53e
Vadim Zainetdinov <vadim.zvf@byndyusoft.com>
2020-01-23 02:14:12 +0300

    :sparkles: add first parsing
    add simple tags parser

--- src/parser/parser.js
+++ src/parser/parser.js
@@ -1,3 +1,114 @@
+function isEmptyString(str) {
+    return str.replace(/\s/g, '').length === 0;
+}
+
+export function getAttributesInfo(attributesString) {
+    // TODO: добавить булевые аттрибуты
+    const attributeRegexp = /(\w+?)\=["']([\w\s]+?)["']/gi;
+    let matches;
+    const attributes = [];
+
+    while ((matches = attributeRegexp.exec(attributesString)) !== null) {
+        attributes.push({ name: matches[1], value: matches[2] });
+    }
+
+    return attributes;
+}
+
+export function getTagInfo(tagString, startIndex, endIndex) {
+    let isCloseTag = /^\/.*$/.test(tagString); // </div>
+    let isSelfClosed = /^.*\/$/.test(tagString); // <div/>
+    let role = 'open';
+    if (isSelfClosed) {
+        role = 'open-close';
+    } else if (isCloseTag) {
+        role = 'close';
+    }
+
+    // TODO: добавить более подробные сорсы для тега
+    const tagNameRegexp = /^\/?([^\s\/]+).*?\/?$/gi;
+    const tagName = tagNameRegexp.exec(tagString)[1];
+
+    const attributesString = tagString.replace(/^\/?([^\s\/]+)/gi, '').replace(/\/$/gi, '');
+
+    return {
+        role,
+        type: 'tag',
+        name: tagName,
+        attributes: getAttributesInfo(attributesString),
+        source: {
+            startIndex,
+            endIndex
+        }
+    };
+}
+
+// Возврращает массив тегов,
+// это еще не ноды, просто список найденных тегов или стоковых нод
+export function getTags(inputValue = '') {
+    if (!inputValue.length) {
+        return [];
+    }
+
+    let currentTag = '';
+    let currentTagStart = 0;
+    let currentTextNode = '';
+    let isTagOpened = false;
+    const foundTagItems = [];
+
+    for (let i = 0; i < inputValue.length; i++) {
+        if (inputValue[i] === '<') {
+            // перед новым тегом была текстовая нода, надо закрыть её
+            if (currentTextNode) {
+                if (!isEmptyString(currentTextNode)) {
+                    // проверяем, вдруг текстовая нода состояла только из пробелов
+                    // TODO: add trimming
+                    foundTagItems.push({ type: 'text', text: currentTextNode });
+                }
+                currentTextNode = '';
+            }
+
+            currentTagStart = i + 1;
+            isTagOpened = true;
+            continue;
+        }
+
+        if (inputValue[i] === '>') {
+            isTagOpened = false;
+            foundTagItems.push(getTagInfo(currentTag, currentTagStart, i - 1));
+            currentTagStart = 0;
+            currentTag = '';
+            continue;
+        }
+
+        if (isTagOpened) {
+            // Мы находимся внутри тега
+            currentTag = `${currentTag}${inputValue[i]}`;
+            continue;
+        } else {
+            // видимо тестовая нода, раз никакой тег не открывался
+            currentTextNode = `${currentTextNode}${inputValue[i]}`;
+        }
+    }
+
+    // прошлись по всей строке но текстовая нода не закрыта, значит на входе была просто строка
+    if (currentTextNode) {
+        if (!isEmptyString(currentTextNode)) {
+            // проверяем, вдруг текстовая нода состояла только из пробелов
+            foundTagItems.push({ type: 'text', text: currentTextNode });
+        }
+    }
+
+    // прошлись по всей строке но один тег не закрылся(
+    // TODO: show error
+    if (currentTag) {
+        foundTagItems.push(getTagInfo(currentTag, currentTagStart, currentTagStart + currentTag.length - 1));
+    }
+
+    return foundTagItems;
+}
+
+// возвращает готовую AST
 function parse() {
     return 1;
 }
--- src/parser/test.js
+++ src/parser/test.js
@@ -1,5 +1,170 @@
-import parser from './parser';
+import parser, { getTags, getTagInfo, getAttributesInfo } from './parser';
 
-test('first test', () => {
-    expect(parser()).toBe(1);
+describe('getTagInfo', () => {
+    test('Should detect open tag', () => {
+        expect(getTagInfo('div', 0, 2)).toStrictEqual({
+            type: 'tag',
+            role: 'open',
+            name: 'div',
+            attributes: [],
+            source: { startIndex: 0, endIndex: 2 }
+        });
+        expect(getTagInfo('div class="wow" id="wow"', 0, 22)).toStrictEqual({
+            type: 'tag',
+            role: 'open',
+            name: 'div',
+            attributes: [
+                {
+                    name: 'class',
+                    value: 'wow'
+                },
+                {
+                    name: 'id',
+                    value: 'wow'
+                }
+            ],
+            source: { startIndex: 0, endIndex: 22 }
+        });
+    });
+    test('Should detect close tag', () => {
+        expect(getTagInfo('/div', 0, 3)).toStrictEqual({
+            type: 'tag',
+            role: 'close',
+            name: 'div',
+            attributes: [],
+            source: { startIndex: 0, endIndex: 3 }
+        });
+        expect(getTagInfo('/div class="wow" id="wow"', 0, 23)).toStrictEqual({
+            type: 'tag',
+            role: 'close',
+            name: 'div',
+            attributes: [
+                {
+                    name: 'class',
+                    value: 'wow'
+                },
+                {
+                    name: 'id',
+                    value: 'wow'
+                }
+            ],
+            source: { startIndex: 0, endIndex: 23 }
+        });
+    });
+    test('Should detect open/close tag', () => {
+        expect(getTagInfo('div/', 0, 3)).toStrictEqual({
+            type: 'tag',
+            role: 'open-close',
+            name: 'div',
+            attributes: [],
+            source: { startIndex: 0, endIndex: 3 }
+        });
+        expect(getTagInfo('div class="wow" id="wow"/', 0, 23)).toStrictEqual({
+            type: 'tag',
+            role: 'open-close',
+            name: 'div',
+            attributes: [
+                {
+                    name: 'class',
+                    value: 'wow'
+                },
+                {
+                    name: 'id',
+                    value: 'wow'
+                }
+            ],
+            source: { startIndex: 0, endIndex: 23 }
+        });
+    });
+});
+
+describe('getAttributesInfo', () => {
+    test('Should get class', () => {
+        expect(getAttributesInfo('class="wow"')).toStrictEqual([
+            {
+                name: 'class',
+                value: 'wow'
+            }
+        ]);
+    });
+    test('Should get class and id', () => {
+        expect(getAttributesInfo('class="wow" id="wow id"')).toStrictEqual([
+            {
+                name: 'class',
+                value: 'wow'
+            },
+            {
+                name: 'id',
+                value: 'wow id'
+            }
+        ]);
+    });
+    test('Should get attribute with single comma', () => {
+        expect(getAttributesInfo("class='wow' id='wow id'")).toStrictEqual([
+            {
+                name: 'class',
+                value: 'wow'
+            },
+            {
+                name: 'id',
+                value: 'wow id'
+            }
+        ]);
+    });
+});
+
+describe('getTags', () => {
+    test('Should return a tag', () => {
+        expect(getTags('<div>')).toStrictEqual([
+            { name: 'div', type: 'tag', role: 'open', attributes: [], source: { startIndex: 1, endIndex: 3 } }
+        ]);
+    });
+    test('Should return two tag', () => {
+        expect(getTags('<div><span>')).toStrictEqual([
+            { name: 'div', type: 'tag', role: 'open', attributes: [], source: { startIndex: 1, endIndex: 3 } },
+            { name: 'span', type: 'tag', role: 'open', attributes: [], source: { startIndex: 6, endIndex: 9 } }
+        ]);
+    });
+    test('Should handle empty string', () => {
+        expect(getTags('')).toStrictEqual([]);
+    });
+    test('Should find a text nodes', () => {
+        expect(getTags('some long text')).toStrictEqual([{ type: 'text', text: 'some long text' }]);
+    });
+    test('Should return tags with text nodes', () => {
+        expect(getTags('<div><span>some text</span></div>')).toStrictEqual([
+            { name: 'div', type: 'tag', role: 'open', attributes: [], source: { startIndex: 1, endIndex: 3 } },
+            { name: 'span', type: 'tag', role: 'open', attributes: [], source: { startIndex: 6, endIndex: 9 } },
+            { type: 'text', text: 'some text' },
+            { name: 'span', type: 'tag', role: 'close', attributes: [], source: { startIndex: 21, endIndex: 25 } },
+            { name: 'div', type: 'tag', role: 'close', attributes: [], source: { startIndex: 28, endIndex: 31 } }
+        ]);
+    });
+    test('Should handle multiline string', () => {
+        expect(
+            getTags(`
+            <div>
+                <span>
+                    some text
+                </span>
+            </div>
+        `)
+        ).toStrictEqual([
+            { name: 'div', type: 'tag', role: 'open', attributes: [], source: { startIndex: 14, endIndex: 16 } },
+            { name: 'span', type: 'tag', role: 'open', attributes: [], source: { startIndex: 36, endIndex: 39 } },
+            {
+                type: 'text',
+                text: `
+                    some text
+                `
+            },
+            { name: 'span', type: 'tag', role: 'close', attributes: [], source: { startIndex: 89, endIndex: 93 } },
+            { name: 'div', type: 'tag', role: 'close', attributes: [], source: { startIndex: 109, endIndex: 112 } }
+        ]);
+    });
+    test('Should handle unclosed tag', () => {
+        expect(getTags('<div')).toStrictEqual([
+            { name: 'div', type: 'tag', role: 'open', attributes: [], source: { startIndex: 1, endIndex: 3 } }
+        ]);
+    });
 });
