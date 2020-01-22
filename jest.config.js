8c3201305dc2c931500efed1fc41f5aaa0a4f5aa
Vadim Zainetdinov <vadim.zvf@byndyusoft.com>
2020-01-22 22:17:40 +0300

    :white_check_mark: add jest
    add simple test config

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,4 @@
 module.exports = {
-    moduleNameMapper: {
-        '\\.(css|pcss)$': 'identity-obj-proxy',
-        '\\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-            '<rootDir>/jest/fileMock.js'
-    },
     roots: ['<rootDir>/src'],
-    modulePathIgnorePatterns: ['/build/.*', '/lib/.*'],
-    setupFiles: ['<rootDir>/jest/setupTests.js'],
-    setupFilesAfterEnv: ['./jest/rtl.setup.js'],
-    coverageDirectory: '<rootDir>/jest/coverage',
-    globals: {
-        PROJECT_ENV: 'development'
-    }
+    modulePathIgnorePatterns: ['/build/.*']
 };
